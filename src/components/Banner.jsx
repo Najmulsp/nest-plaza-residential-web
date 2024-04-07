c2dde0d32471cd9deb00a5f615fdfd4df28250a4
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-04-08 03:00:35 +0600

    added banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,48 @@
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, A11y } from 'swiper/modules';
+import 'swiper/css';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Banner = () => {
-    return (
-        <div>
+  return (
+    <Swiper
+    modules={[Navigation, Pagination, A11y]}
+    spaceBetween={50}
+    slidesPerView={1}
+    navigation
+    pagination={{ clickable: true }}
+    scrollbar={{ draggable: true }}
+    //   onSlideChange={() => console.log("slide change")}
+    //   onSwiper={(swiper) => console.log(swiper)}
+    >
+        <SwiperSlide>
             
-        </div>
-    );
+            <div className='bg-[url("https://i.ibb.co/XCJHxp6/vacation-rental.webp")] bg-no-repeat bg-cover rounded-2xl  bg-[#12132D] w-full h-[550px] '>
+                <h1 className='text-4xl font-extrabold text-black text-center pt-32'>Welcome to Nest Plaza</h1>
+                <p className='text-xl font-semibold text-black text-center pt-8'>The home you are dreaming of is waiting for you</p>
+            </div>
+            {/* <img className='w-full h-[550px] rounded-2xl  mx-auto' src="https://i.ibb.co/XCJHxp6/vacation-rental.webp" alt="" /> */}
+           
+        </SwiperSlide>
+        <SwiperSlide>
+        <img className='w-full h-[550px] rounded-2xl  mx-auto' src="https://i.ibb.co/rHQn59d/homes-3.webp" alt="" />
+            
+        </SwiperSlide>
+        <SwiperSlide>
+        <img className='w-full h-[550px] rounded-2xl  mx-auto' src="https://i.ibb.co/hRYLQ1q/apartment1.jpg" alt="" />
+            
+        </SwiperSlide>
+        <SwiperSlide>
+        <img className='w-full h-[550px] rounded-2xl mx-auto' src="https://i.ibb.co/CJbGVYt/family-home.jpg" alt="" />
+           
+        </SwiperSlide>
+      ...
+    </Swiper>
+  );
 };
 
 export default Banner;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import Banner from "./Banner";
 const Home = () => {
     return (
         <div>
-            <h2>this is home</h2>
+            
             <Banner></Banner>
         </div>
     );
